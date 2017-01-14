e36fcf47fe4ccc30a82163c77f55cb06f6844cd4
Michael Schwartz <mikethedj4@yahoo.com>
2017-01-14 15:19:44 -0600

    view site in only https

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+// If domain is HTTP
+var site = window.location;
+site = site.toString();
+if (site.substring(0, 7) === "http://") {
+  window.location.href = "https://" + site.substring(7, site.length);
+}
+
 var videoElement = document.querySelector("video"),
     videoSelect  = document.querySelector("#videoSource"),
     canvas       = document.getElementById("canvas"),
